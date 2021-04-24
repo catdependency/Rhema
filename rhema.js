8dec1df85a24c5402d7e0e669021a8fcef0ca79f
catdependency <83100382+catdependency@users.noreply.github.com>
2021-04-23 17:26:01 -0700

    A Rhema Webpage
    My first time committing

---
+++ rhema.js
@@ -0,0 +1,15 @@
+function refreshIframe() {
+    var ifr = document.getElementsByName('Bible-Gateway')[0];
+    ifr.src = ifr.src;
+}
+function refreshIframe1() {
+    var ifr = document.getElementsByName('hd')[0];
+    ifr.src = ifr.src;
+}
+function refreshIframe2() {
+    var ifr = document.getElementsByName('sanders')[0];
+    ifr.src = ifr.src;
+}function refreshIframe3() {
+    var ifr = document.getElementsByName('Random-Chapter')[0];
+    ifr.src = ifr.src;
+}
